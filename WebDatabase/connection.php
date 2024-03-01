70c4dec2483eee7417248fa83a8ff37bb610dbe0
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2024-03-01 07:35:37 +0100

    Delete WebDatabase/connection.php

--- WebDatabase/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $server="localhost";
-    $username="programma";
-    $password="12345";
-    $connection=new PDO("mysql:host=$server;dbname=speedrunning",$username,$password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
